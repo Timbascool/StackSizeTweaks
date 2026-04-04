2b521dd14c2b798706c8e5556a066ff181b44fe7
Timbascool <rametukko@gmail.com>
2026-04-04 16:28:21 +0300

    Fixes

--- src/main/java/me/timbas/stacksizetweaks/mixin/ContainersMixin.java
+++ src/main/java/me/timbas/stacksizetweaks/mixin/ContainersMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 
-// Make containers drop items with higher stack sizes
+// Increase stack size containers drop to avoid lag
 @Mixin(Containers.class)
 public class ContainersMixin {
     @ModifyExpressionValue(
