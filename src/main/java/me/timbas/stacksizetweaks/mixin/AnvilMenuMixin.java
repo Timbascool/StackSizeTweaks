81a580200a0087c91d7bd0c5995278018bb6f6d3
Timbascool <rametukko@gmail.com>
2026-04-05 06:56:06 +0300

    Changed version and added enchanted books

--- src/main/java/me/timbas/stacksizetweaks/mixin/AnvilMenuMixin.java
+++ src/main/java/me/timbas/stacksizetweaks/mixin/AnvilMenuMixin.java
@@ -4,17 +4,31 @@ package me.timbas.stacksizetweaks.mixin;
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilMenu.class)
 public class AnvilMenuMixin {
-    @Redirect(method = "onTake",
-            at = @At(value = "INVOKE",
+    @Redirect(
+            method = "onTake",
+            at = @At(
+                    value = "INVOKE",
                     target = "Lnet/minecraft/world/Container;setItem(ILnet/minecraft/world/item/ItemStack;)V",
-                    ordinal = 3))
-    void fixStackedEnchantedBooksBeingDeleted(Container inputSlots, int i, ItemStack itemStack) {
-        inputSlots.getItem(1).shrink(1);
+                    ordinal = 2
+            )
+    )
+    void fixStackedEnchantedBooksBeingDeleted(Container container, int i, ItemStack itemStack) {
+        if (i == 1 && itemStack.isEmpty()) {
+            ItemStack ingredient = container.getItem(1);
+
+            if (ingredient.is(Items.ENCHANTED_BOOK) && ingredient.getCount() > 1) {
+                ingredient.shrink(1);
+                container.setItem(1, ingredient);
+            }
+        }
+
+
     }
 }
