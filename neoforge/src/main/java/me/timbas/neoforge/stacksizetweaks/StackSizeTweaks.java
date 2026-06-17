ae2d64cb04da60975fba2b7f70757074a186e3e3
Timbascool <rametukko@gmail.com>
2026-06-17 12:07:56 +0300

    Fix server crash on neoforge

--- neoforge/src/main/java/me/timbas/neoforge/stacksizetweaks/StackSizeTweaks.java
+++ neoforge/src/main/java/me/timbas/neoforge/stacksizetweaks/StackSizeTweaks.java
@@ -1,27 +1,24 @@
 package me.timbas.neoforge.stacksizetweaks;
 
 import me.timbas.stacksizetweaks.StackSizeHelper;
-import me.timbas.stacksizetweaks.StackSizeTweaksConfig;
-import me.timbas.stacksizetweaks.StackSizeTweaksConfigScreen;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
 
 @Mod(me.timbas.stacksizetweaks.StackSizeTweaks.MOD_ID)
 public final class StackSizeTweaks {
     public StackSizeTweaks(IEventBus modBus) {
 
-        ModLoadingContext.get().registerExtensionPoint(
-                IConfigScreenFactory.class,
-                () -> (client, parent) -> StackSizeTweaksConfigScreen.createScreen(parent)
-        );
-
+        if (FMLEnvironment.getDist() == Dist.CLIENT)
+        {
+            StackSizeTweaksClient.ClientInit();
+        }
 
         me.timbas.stacksizetweaks.StackSizeTweaks.init();
 
