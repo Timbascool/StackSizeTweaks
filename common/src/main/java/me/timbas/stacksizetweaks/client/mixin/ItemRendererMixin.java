bf67e0bbf5ce8eeecf15c3b4bc4c278edae164c0
Timbascool <rametukko@gmail.com>
2026-07-07 22:37:54 +0300

    New font option

--- common/src/main/java/me/timbas/stacksizetweaks/client/mixin/ItemRendererMixin.java
+++ common/src/main/java/me/timbas/stacksizetweaks/client/mixin/ItemRendererMixin.java
@@ -1,5 +1,6 @@
 package me.timbas.stacksizetweaks.client.mixin;
 
+import me.timbas.stacksizetweaks.FontOption;
 import me.timbas.stacksizetweaks.StackSizeTweaks;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
@@ -21,7 +22,7 @@ public abstract class ItemRendererMixin {
     private void changeItemCountText(Font font, ItemStack itemStack, int x, int y, @Nullable String countText, CallbackInfo ci) {
         if (itemStack.getCount() != 1 || countText != null) {
             String newText = stacksizetweaks$formatCount(itemStack.getCount(), StackSizeTweaks.CONFIG.shortenItemAmounts);
-            Component renderedText = stacksizetweaks$makeText(newText, StackSizeTweaks.CONFIG.useCustomFont);
+            Component renderedText = stacksizetweaks$makeText(newText, StackSizeTweaks.CONFIG.customFont);
 
             ((GuiGraphicsExtractor) (Object) this).text(font, renderedText, x + 17 - font.width(renderedText), y + 9, -1, true);
         }
@@ -30,20 +31,31 @@ public abstract class ItemRendererMixin {
     }
 
     @Unique
-    private static final FontDescription CUSTOM_FONT =
+    private static final FontDescription SMALL_FONT =
             new FontDescription.Resource(
-                    Identifier.fromNamespaceAndPath(StackSizeTweaks.MOD_ID, "inventory_font")
+                    Identifier.fromNamespaceAndPath(StackSizeTweaks.MOD_ID, "small_font")
             );
 
     @Unique
-    private static Component stacksizetweaks$makeText(String text, boolean useCustomFont) {
+    private static final FontDescription TINY_FONT =
+            new FontDescription.Resource(
+                    Identifier.fromNamespaceAndPath(StackSizeTweaks.MOD_ID, "tiny_font")
+            );
 
-        if (useCustomFont) {
-            return Component.literal(text)
-                    .withStyle(style -> style.withFont(CUSTOM_FONT));
-        } else {
-            return Component.literal(text);
-        }
+    @Unique
+    private static Component stacksizetweaks$makeText(String text, FontOption fontOption) {
+
+        return switch (fontOption) {
+            case Vanilla -> Component.literal(text);
+
+            case Small -> Component.literal(text)
+                    .withStyle(style -> style.withFont(SMALL_FONT));
+
+            case Tiny -> Component.literal(text)
+                    .withStyle(style -> style.withFont(TINY_FONT));
+
+            case null -> Component.literal(text);
+        };
     }
 
     @Unique
