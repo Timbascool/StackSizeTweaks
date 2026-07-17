921779b648ac4743184ad0b83c4d12ed0da0aac8
Timbascool <rametukko@gmail.com>
2026-07-17 17:15:07 +0300

    Add neoforge specific mixins

---
+++ neoforge/src/main/java/me/timbas/neoforge/stacksizetweaks/mixin/EntityEquipmentInvWrapperMixin.java
@@ -0,0 +1,19 @@
+package me.timbas.neoforge.stacksizetweaks.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import me.timbas.stacksizetweaks.StackSizeTweaks;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.neoforged.neoforge.items.wrapper.EntityEquipmentInvWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EntityEquipmentInvWrapper.class)
+public class EntityEquipmentInvWrapperMixin {
+    @ModifyReturnValue(
+            method = "getSlotLimit",
+            at = @At("RETURN")
+    )
+    private int increaseMaxStackSize(int original) {
+        return original == 99 ? StackSizeTweaks.ABSOLUTE_MAX_STACK_SIZE : original;
+    }
+}
