e959992480a150d11438703bd9aa7cc086a3936e
Timbascool <rametukko@gmail.com>
2026-07-17 14:17:40 +0300

    Move tooltip to client side in fabric

--- fabric/src/main/java/me/timbas/fabric/stacksizetweaks/StackSizeTweaks.java
+++ fabric/src/main/java/me/timbas/fabric/stacksizetweaks/StackSizeTweaks.java
@@ -2,11 +2,8 @@ package me.timbas.fabric.stacksizetweaks;
 
 import me.timbas.stacksizetweaks.StackSizeHelper;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
 
 public final class StackSizeTweaks implements ModInitializer {
     @Override
@@ -16,16 +13,6 @@ public final class StackSizeTweaks implements ModInitializer {
         DefaultItemComponentEvents.MODIFY.register(modifyContext -> {
             modifyContext.modify(item -> true, (builder, item) -> builder
                     .set(DataComponents.MAX_STACK_SIZE, StackSizeHelper.getMaxStackSize(item, item.components())));
-        });
-
-        ItemTooltipCallback.EVENT.register((stack, context, tooltipType, lines) -> {
-            int count = stack.getCount();
-
-            if (me.timbas.stacksizetweaks.StackSizeTweaks.CONFIG.amountTooltip && count > 999)
-            {
-                String amount = String.format("%,d", count);
-                lines.add(1, Component.literal(amount).withStyle(ChatFormatting.GRAY));
-            }
         });
     }
 }
