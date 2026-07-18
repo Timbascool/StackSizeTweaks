366203e1cf0c22566d766ce70a3d6e771b564447
Timbascool <rametukko@gmail.com>
2026-07-18 14:08:58 +0300

    Fix neoforge mixins

---
+++ neoforge/src/main/java/me/timbas/neoforge/stacksizetweaks/mixin/ItemAccessResourceHandlerMixin.java
@@ -0,0 +1,20 @@
+package me.timbas.neoforge.stacksizetweaks.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import me.timbas.stacksizetweaks.StackSizeTweaks;
+import net.neoforged.neoforge.transfer.item.ItemAccessItemHandler;
+import net.neoforged.neoforge.transfer.item.ItemResource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ItemAccessItemHandler.class)
+public class ItemAccessResourceHandlerMixin {
+    @ModifyReturnValue(
+            method = "getCapacity*",
+            at = @At("RETURN")
+    )
+    private int increaseMaxStackSize(int original, ItemResource resource) {
+        return resource.isEmpty() ? StackSizeTweaks.ABSOLUTE_MAX_STACK_SIZE : Math.min(resource.getMaxStackSize(), StackSizeTweaks.ABSOLUTE_MAX_STACK_SIZE);
+    }
+}
+
